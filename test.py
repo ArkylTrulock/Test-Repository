17621849e1c34cd424ea9f259ad19e25aa39f320
Vernyuy1981 <axel201120@github.com>
2024-03-14 22:25:47 +0000

    Test-repo, 1st commit

---
+++ test.py
@@ -0,0 +1,5 @@
+num_day = int(input("How many days worked this month? "))
+pay_per_day = float(input("How much is your pay per day? "))
+salary = num_day * pay_per_day
+
+print(f"My salary for the month is £{salary:.0f}")
